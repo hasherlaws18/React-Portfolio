f65ca449d6bfb89883abbbb1ef1fc88d0f1fa48c
hasherlaws18 <houstonasherlaws@gmail.com>
2023-05-03 21:01:28 -0500

    new changes to Components

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,2 +1,14 @@
 import React from "react";
 
+function Aboutme(){
+    return(
+        <div>
+            <h1>About me</h1>
+            <p>Hello my name is Houston Asher-Laws and right now I am pursing a job as a Junior we devolper.
+                I have always had a intrest into coding and web development which made me want to purse a career in this field.
+                Over the past six months I have been doing a coding bootcamp and this bootcamp I have learned many types of skills that would make a great addation to any company. I have learned Node.js, React, MYSQL, NOSQL, and many more languages.
+            </p>
+        </div>
+    )
+}
+export default Aboutme;
